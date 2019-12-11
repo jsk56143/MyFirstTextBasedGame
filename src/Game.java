ab750066200e12cfb05917299d261b92075c1f11
jsk56143 <jsk56143@uga.edu>
2019-12-10 21:10:43 -0500

    Implemented a display map of the game with checks for invalid commands and trying to go out of bounds

--- src/Game.java
+++ src/Game.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 //Mae Game
 public class Game {
 
-    //private String[][] displayArray = new String[3][3];
-    //private int[][] hiddenArray = new int[3][3];
+    private String[][] displayArray = new String[3][3];
+    private int[][] hiddenArray = new int[3][3]; //Do I need this?
     private int currentRow, currentCol;
     private String command;
 
@@ -12,17 +12,34 @@ public class Game {
         currentRow = 0;
         currentCol = 0;
         command = "";
+        for (int i = 0; i < displayArray.length; i++) {
+            for (int j = 0; j < displayArray[0].length; j++) {
+                if (j == 0) {
+                    displayArray[i][j] = "|   |";
+                    if (i == 0) {
+                        displayArray[i][j] = "| X |";
+                    }
+                } else {
+                    displayArray[i][j] = "   |";
+                }
+                hiddenArray[i][j] = 0;
+            }
+        }
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Game game = new Game();
         do {
+            for (int i = 0; i < game.displayArray.length; i++) {
+                for (int j = 0; j < game.displayArray[0].length; j++) {
+                    System.out.print(game.displayArray[i][j]);
+                }
+                System.out.println();
+            }
             System.out.print("What do you want to do? ");
             game.command = input.nextLine();
             game.action(game.command);
-            System.out.println("Row: " + game.currentRow);
-            System.out.println("Col: " + game.currentCol);
         }
         while (!game.command.equalsIgnoreCase("quit"));
         input.close();
@@ -35,29 +52,82 @@ public class Game {
      */
     private void action(String command) {
         boolean moveCondition;
+        int count = 0; //Determines if user entered a valid command
         if (command.equalsIgnoreCase("Move Right")) {
             moveCondition = moveCheck("right");
             if (moveCondition) {
-                currentCol++;
+                if (currentCol == 0) {
+                    displayArray[currentRow][currentCol] = "|   |";
+                } else {
+                    displayArray[currentRow][currentCol] = "   |";
+                }
+                hiddenArray[currentRow][currentCol] = 0;
+                hiddenArray[currentRow][currentCol++] = 1;
+                displayArray[currentRow][currentCol] = " X |";
+            } else {
+                System.out.println("Can't move in that direction.");
             }
+            count++;
         }
         if (command.equalsIgnoreCase("Move Left")) {
             moveCondition = moveCheck("left");
             if (moveCondition) {
-                currentCol--;
+                if (currentCol - 1 == 0) {
+                    displayArray[currentRow][currentCol] = "   |";
+                    hiddenArray[currentRow][currentCol] = 0;
+                    hiddenArray[currentRow][currentCol--] = 1;
+                    displayArray[currentRow][currentCol] = "| X |";
+                } else {
+                    displayArray[currentRow][currentCol] = "   |";
+                    hiddenArray[currentRow][currentCol] = 0;
+                    hiddenArray[currentRow][currentCol--] = 1;
+                    displayArray[currentRow][currentCol] = " X |";
+                }
+            } else {
+                System.out.println("Can't move in that direction.");
             }
+            count++;
         }
         if (command.equalsIgnoreCase("Move Up")) {
             moveCondition = moveCheck("up");
             if (moveCondition) {
-                currentRow--;
+                if (currentCol == 0) {
+                    displayArray[currentRow][currentCol] = "|   |";
+                    hiddenArray[currentRow][currentCol] = 0;
+                    hiddenArray[currentRow--][currentCol] = 1;
+                    displayArray[currentRow][currentCol] = "| X |";
+                } else {
+                    displayArray[currentRow][currentCol] = "   |";
+                    hiddenArray[currentRow][currentCol] = 0;
+                    hiddenArray[currentRow--][currentCol] = 1;
+                    displayArray[currentRow][currentCol] = " X |";
+                }
+            } else {
+                System.out.println("Can't move in that direction.");
             }
+            count++;
         }
         if (command.equalsIgnoreCase("Move Down")) {
             moveCondition = moveCheck("down");
             if (moveCondition) {
-                currentRow++;
+                if (currentCol == 0) {
+                    displayArray[currentRow][currentCol] = "|   |";
+                    hiddenArray[currentRow][currentCol] = 0;
+                    hiddenArray[currentRow++][currentCol] = 1;
+                    displayArray[currentRow][currentCol] = "| X |";
+                } else {
+                    displayArray[currentRow][currentCol] = "   |";
+                    hiddenArray[currentRow][currentCol] = 0;
+                    hiddenArray[currentRow++][currentCol] = 1;
+                    displayArray[currentRow][currentCol] = " X |";
+                }
+            } else {
+                System.out.println("Can't move in that direction.");
             }
+            count++;
+        }
+        if (count == 0 && !command.equalsIgnoreCase("quit")) {
+            System.out.println("Invalid command.");
         }
     }
 
