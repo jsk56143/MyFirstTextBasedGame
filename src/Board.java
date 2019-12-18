2e54341cdc8e35bf67ee1fc0e2ac89f1a3b5bb4a
jsk56143 <jsk56143@uga.edu>
2019-12-18 17:40:54 -0500

    Corrected errors in Board class

--- src/Board.java
+++ src/Board.java
@@ -4,7 +4,7 @@ public class Board {
     private String[][] displayArray = new String[3][3];
     private int[][] hiddenArray = new int[3][3]; //Do I need this?
     private int currentRow, currentCol;
-    private String command;
+    protected String command;
 
     /*
      * The constructor creates the game object and initializes the
@@ -18,17 +18,14 @@ public class Board {
         command = "";
     }
 
-    protected void printBoard() {
-        Game game = new Game();
+    protected void initializeBoard() {
         for (int i = 0; i < displayArray.length; i++) {
             for (int j = 0; j < displayArray[0].length; j++) {
-                if (i == 0 && j == 0 && game.getFirstTime()) { //Initializes player's token at 0,0
+                if (i == 0 && j == 0) { //Initializes player's token at 0,0
                     displayArray[i][j] = "| X |";
-                    game.setFirstTime(false);
-                }
-                if (j == 0 && !game.getFirstTime()) { //If first column and not initializing player's token
+                } else if (j == 0 && i > 0) { //If first column and not initializing player's token
                     displayArray[i][j] = "|   |";
-                } else { //If column other than 0
+                } else if (j > 0) { //If column other than 0
                     displayArray[i][j] = "   |";
                 }
                 hiddenArray[i][j] = 0;
@@ -36,6 +33,121 @@ public class Board {
         }
     }
 
+    protected void printBoard() {
+        for (int i = 0; i < displayArray.length; i++) {
+            for (int j = 0; j < displayArray[0].length; j++) {
+                System.out.print(displayArray[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+     * A method that determines if the user inputted a valid command,
+     * checks the if the command can be executed, and executes the
+     * command if it can be done.
+     */
+    protected void action(String command) {
+        boolean moveCondition;
+        int count = 0; //Determines if user entered a valid command
+        if (command.equalsIgnoreCase("Move Right")) {
+            moveCondition = moveCheck("right");
+            if (moveCondition) {
+                if (currentCol == 0) {
+                    displayArray[currentRow][currentCol] = "|   |";
+                } else {
+                    displayArray[currentRow][currentCol] = "   |";
+                }
+                hiddenArray[currentRow][currentCol] = 0;
+                hiddenArray[currentRow][currentCol++] = 1;
+                displayArray[currentRow][currentCol] = " X |";
+            } else {
+                System.out.println("Can't move in that direction.");
+            }
+            count++;
+        }
+        if (command.equalsIgnoreCase("Move Left")) {
+            moveCondition = moveCheck("left");
+            if (moveCondition) {
+                if (currentCol - 1 == 0) {
+                    displayArray[currentRow][currentCol] = "   |";
+                    hiddenArray[currentRow][currentCol] = 0;
+                    hiddenArray[currentRow][currentCol--] = 1;
+                    displayArray[currentRow][currentCol] = "| X |";
+                } else {
+                    displayArray[currentRow][currentCol] = "   |";
+                    hiddenArray[currentRow][currentCol] = 0;
+                    hiddenArray[currentRow][currentCol--] = 1;
+                    displayArray[currentRow][currentCol] = " X |";
+                }
+            } else {
+                System.out.println("Can't move in that direction.");
+            }
+            count++;
+        }
+        if (command.equalsIgnoreCase("Move Up")) {
+            moveCondition = moveCheck("up");
+            if (moveCondition) {
+                if (currentCol == 0) {
+                    displayArray[currentRow][currentCol] = "|   |";
+                    hiddenArray[currentRow][currentCol] = 0;
+                    hiddenArray[currentRow--][currentCol] = 1;
+                    displayArray[currentRow][currentCol] = "| X |";
+                } else {
+                    displayArray[currentRow][currentCol] = "   |";
+                    hiddenArray[currentRow][currentCol] = 0;
+                    hiddenArray[currentRow--][currentCol] = 1;
+                    displayArray[currentRow][currentCol] = " X |";
+                }
+            } else {
+                System.out.println("Can't move in that direction.");
+            }
+            count++;
+        }
+        if (command.equalsIgnoreCase("Move Down")) {
+            moveCondition = moveCheck("down");
+            if (moveCondition) {
+                if (currentCol == 0) {
+                    displayArray[currentRow][currentCol] = "|   |";
+                    hiddenArray[currentRow][currentCol] = 0;
+                    hiddenArray[currentRow++][currentCol] = 1;
+                    displayArray[currentRow][currentCol] = "| X |";
+                } else {
+                    displayArray[currentRow][currentCol] = "   |";
+                    hiddenArray[currentRow][currentCol] = 0;
+                    hiddenArray[currentRow++][currentCol] = 1;
+                    displayArray[currentRow][currentCol] = " X |";
+                }
+            } else {
+                System.out.println("Can't move in that direction.");
+            }
+            count++;
+        }
+        if (count == 0 && !command.equalsIgnoreCase("quit")) {
+            System.out.println("Invalid command.");
+        }
+    }
+
+    /*
+     * A  method that checks if the user can proceed in the
+     * specified direction and returns true if it can, false
+     * otherwise.
+     */
+    private boolean moveCheck(String direction) {
+        if (direction.equals("right") && currentCol + 1 >= 3) {
+            return false;
+        }
+        if (direction.equals("left") && currentCol - 1 < 0) {
+            return false;
+        }
+        if (direction.equals("up") && currentRow - 1 < 0) {
+            return false;
+        }
+        if (direction.equals("down") && currentRow + 1 >= 3) {
+            return false;
+        }
+        return true;
+    }
 
 
 
--- src/Game.java
+++ src/Game.java
@@ -3,10 +3,6 @@ import java.util.Scanner;
 //Make Game
 public class Game {
 
-    protected Game() {
-        boolean firstTime = true;
-    }
-
     /*
      * The main method, the entry-point to my program, creates the game object,
      * prints the board out to standard output, asks the user for input, and
@@ -15,139 +11,17 @@ public class Game {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Board board = new Board();
+        board.initializeBoard();
         do {
             board.printBoard();
-            /*
-            for (int i = 0; i < board.displayArray.length; i++) {
-                for (int j = 0; j < board.displayArray[0].length; j++) {
-                    System.out.print(game.displayArray[i][j]);
-                }
-                System.out.println();
-            }
-            */
-
             //Print out description of room
             System.out.print("What do you want to do? ");
-            game.command = input.nextLine(); //Stores the input
-            game.action(game.command); //Does the action according to the input
+            board.command = input.nextLine(); //Stores the input
+            board.action(board.command); //Does the action according to the input
         }
-        while (!game.command.equalsIgnoreCase("quit")); //Keep going unless player enters "quit"
+        while (!board.command.equalsIgnoreCase("quit")); //Keep going unless player enters "quit"
         input.close();
     }
 
-    public boolean getFirstTime() {
-        return firstTime;
-    }
-
-    public void setFirstTime(boolean condition) {
-        this.firstTime = condition;
-    }
-
-    /*
-     * A method that determines if the user inputted a valid command,
-     * checks the if the command can be executed, and executes the
-     * command if it can be done.
-     */
-    private void action(String command) {
-        boolean moveCondition;
-        int count = 0; //Determines if user entered a valid command
-        if (command.equalsIgnoreCase("Move Right")) {
-            moveCondition = moveCheck("right");
-            if (moveCondition) {
-                if (currentCol == 0) {
-                    displayArray[currentRow][currentCol] = "|   |";
-                } else {
-                    displayArray[currentRow][currentCol] = "   |";
-                }
-                hiddenArray[currentRow][currentCol] = 0;
-                hiddenArray[currentRow][currentCol++] = 1;
-                displayArray[currentRow][currentCol] = " X |";
-            } else {
-                System.out.println("Can't move in that direction.");
-            }
-            count++;
-        }
-        if (command.equalsIgnoreCase("Move Left")) {
-            moveCondition = moveCheck("left");
-            if (moveCondition) {
-                if (currentCol - 1 == 0) {
-                    displayArray[currentRow][currentCol] = "   |";
-                    hiddenArray[currentRow][currentCol] = 0;
-                    hiddenArray[currentRow][currentCol--] = 1;
-                    displayArray[currentRow][currentCol] = "| X |";
-                } else {
-                    displayArray[currentRow][currentCol] = "   |";
-                    hiddenArray[currentRow][currentCol] = 0;
-                    hiddenArray[currentRow][currentCol--] = 1;
-                    displayArray[currentRow][currentCol] = " X |";
-                }
-            } else {
-                System.out.println("Can't move in that direction.");
-            }
-            count++;
-        }
-        if (command.equalsIgnoreCase("Move Up")) {
-            moveCondition = moveCheck("up");
-            if (moveCondition) {
-                if (currentCol == 0) {
-                    displayArray[currentRow][currentCol] = "|   |";
-                    hiddenArray[currentRow][currentCol] = 0;
-                    hiddenArray[currentRow--][currentCol] = 1;
-                    displayArray[currentRow][currentCol] = "| X |";
-                } else {
-                    displayArray[currentRow][currentCol] = "   |";
-                    hiddenArray[currentRow][currentCol] = 0;
-                    hiddenArray[currentRow--][currentCol] = 1;
-                    displayArray[currentRow][currentCol] = " X |";
-                }
-            } else {
-                System.out.println("Can't move in that direction.");
-            }
-            count++;
-        }
-        if (command.equalsIgnoreCase("Move Down")) {
-            moveCondition = moveCheck("down");
-            if (moveCondition) {
-                if (currentCol == 0) {
-                    displayArray[currentRow][currentCol] = "|   |";
-                    hiddenArray[currentRow][currentCol] = 0;
-                    hiddenArray[currentRow++][currentCol] = 1;
-                    displayArray[currentRow][currentCol] = "| X |";
-                } else {
-                    displayArray[currentRow][currentCol] = "   |";
-                    hiddenArray[currentRow][currentCol] = 0;
-                    hiddenArray[currentRow++][currentCol] = 1;
-                    displayArray[currentRow][currentCol] = " X |";
-                }
-            } else {
-                System.out.println("Can't move in that direction.");
-            }
-            count++;
-        }
-        if (count == 0 && !command.equalsIgnoreCase("quit")) {
-            System.out.println("Invalid command.");
-        }
-    }
-
-    /*
-     * A  method that checks if the user can proceed in the
-     * specified direction and returns true if it can, false
-     * otherwise.
-     */
-    private boolean moveCheck(String direction) {
-        if (direction.equals("right") && currentCol + 1 >= 3) {
-            return false;
-        }
-        if (direction.equals("left") && currentCol - 1 < 0) {
-            return false;
-        }
-        if (direction.equals("up") && currentRow - 1 < 0) {
-            return false;
-        }
-        if (direction.equals("down") && currentRow + 1 >= 3) {
-            return false;
-        }
-        return true;
-    }
 
 }
